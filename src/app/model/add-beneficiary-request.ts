410d892e31ce9d2e567df2c4c73ba1723444ea05
Oliver Pagalanan <og.pagalanan@yahoo.com>
2022-03-21 16:14:32 -0400

    latest changes

--- src/app/model/add-beneficiary-request.ts
+++ src/app/model/add-beneficiary-request.ts
@@ -2,5 +2,5 @@ import { AccountType } from '../enums/account-type';
 
 export class AddBeneficiaryRequest {
   accountNumber: number | null = null;
-  accountType: AccountType | null = AccountType.CURRENT;
+  accountType: AccountType | null = AccountType.ACCOUNT_CURRENT;
 }
